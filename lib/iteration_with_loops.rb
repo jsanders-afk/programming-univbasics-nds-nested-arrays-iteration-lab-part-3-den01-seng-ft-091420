fc8328fd38aed91b93a71bc91eab9918e45bf2ca
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-02 05:15:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,11 +10,13 @@ def join_nested_strings(src)
   message = []
   row_index = 0
     while row_index < src.length do
-  end
-  
-  
-  
-  
+    element_index = 0
+      while element_index < src[row_index].length do
+        if src[row_index][element_index] = String
+           src[row_index][element_index] << message
+      end
+    row_index += 1 
+    end
   message
 end
 
